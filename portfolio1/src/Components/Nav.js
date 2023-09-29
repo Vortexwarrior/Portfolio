6dc6f740405c6ff7e76ebc2a3b508e139ec2c309
christian guzman <Ngodshands143@gmail.com>
2023-09-28 21:12:19 -0500

    added to nav.js

--- portfolio1/src/Components/Nav.js
+++ portfolio1/src/Components/Nav.js
@@ -1,1 +1,26 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Header from './header.js';
+import Navigation from './navigation.js';
+import AboutMe from './aboutme.js';
+import PortfolioPage from './portfoliopage.js';
+import Footer from './footer.js';
+import Portfolio from './portfolio.js';
+
+function App() {
+  return (
+    <Router>
+      <Header />
+      <Navigation />
+      <Switch>
+        <Route path="/" exact component={AboutMe} />
+        <Route path="/portfolio" component={PortfolioPage} />
+      </Switch>
+      <Portfolio />
+      <Footer />
+    </Router>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
